71a615bb7b6e0e79d8ff828407f930c66766aea8
Arthur Valadares <arthur00@gmail.com>
2016-05-26 15:00:04 -0700

    all.py gets all pcc sets automatically

--- datamodel/all.py
+++ datamodel/all.py
@@ -3,52 +3,41 @@ Created on Apr 19, 2016
 
 @author: Rohan Achar
 '''
-import datamodel.common.datamodel as common
-import datamodel.carpedestrian.datamodel as carpedestrian
-import datamodel.akshatp.datamodel as akshatp
-import datamodel.nodesim.datamodel as nodesim
-import datamodel.amazoncar.mydatamodel as amazoncar
-import datamodel.uberCar_Xi.datamodel as xi
-import datamodel.wpcar.datamodel as wp
-import datamodel.silverCar.datamodel as silver
+import pkgutil
+import importlib
+import inspect
 
-DATAMODEL_TYPES = [
-                                        carpedestrian.Car,
-                                        carpedestrian.Pedestrian,
-                                        carpedestrian.ActiveCar,
-                                        carpedestrian.InactiveCar,
-                                        carpedestrian.Walker,
-                                        carpedestrian.PedestrianInDanger,
-                                        carpedestrian.StoppedPedestrian,
-                                        carpedestrian.CarAndPedestrianNearby,
-                                        akshatp.Car_akshatp,
-                                        akshatp.Pedestrian_akshatp,
-                                        akshatp.ActiveCar_akshatp,
-                                        akshatp.InactiveCar_akshatp,
-                                        akshatp.Walker_akshatp,
-                                        akshatp.StoppedPedestrian_akshatp,
-                                        akshatp.CarAndPedestrianNearby_akshatp,
-                                        akshatp.PedestrianHasAvodiedCollision_akshatp,
-                                        nodesim.Waypoint,
-                                        nodesim.BusinessNode,
-                                        nodesim.ResidentialNode,
-                                        nodesim.Node,
-                                        nodesim.RouteRequest,
-                                        nodesim.Route,
-                                        nodesim.Road,
-                                        nodesim.Edge,
-                                        common.Vehicle,
-                                        amazoncar.AmazonActiveCar,
-                                        amazoncar.AmazonInactiveCar,
-                                        xi.ActiveUberCar,
-                                        xi.CarAndCarInfront,
-                                        xi.InactiveUberCar,
-                                        xi.UberCar,
-                                        wp.AssignedWPCar,
-                                        wp.FreeWPCar,
-                                        wp.WPCar,
-                                        silver.ActiveSilverCar,
-                                        silver.CarNearCar,
-                                        silver.InactiveSilverCar,
-                                        silver.SilverCar
-                                ]
+def load_all_sets(reload_modules=False):
+    global DATAMODEL_TYPES
+    DATAMODEL_TYPES = []
+
+    module_list = []
+    datamodel_list = []
+
+    for _, name, ispkg in pkgutil.iter_modules(['datamodel']):
+        if ispkg:
+            try:
+                mod = importlib.import_module('datamodel.' + name)
+                module_list.append(mod)
+                if reload_modules:
+                    reload(mod)
+            except:
+                print "Failed to load module datamodel.%s" % name
+                raise
+
+    for module in module_list:
+        for _, name, _ in pkgutil.iter_modules(module.__path__):
+            try:
+                mod = importlib.import_module(module.__name__ + '.' + name)
+                datamodel_list.append(mod)
+                if reload_modules:
+                    reload(mod)
+            except:
+                print "Failed to load module %s.%s" % (module.__name__, name)
+
+    for module in datamodel_list:
+        for name, cls in inspect.getmembers(module, inspect.isclass):
+            if hasattr(cls, "__dependent_type__"):
+                DATAMODEL_TYPES.append(cls)
+
+load_all_sets()
