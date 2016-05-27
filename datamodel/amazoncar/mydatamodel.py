5460071c90baf81db4f5c4117b192c9a65e472f7
Arthur Valadares <arthur00@gmail.com>
2016-05-26 19:06:37 -0700

    part3

--- datamodel/amazoncar/mydatamodel.py
+++ datamodel/amazoncar/mydatamodel.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 LOG_HEADER = "[DATAMODEL]"
 
 @pcc_set
-class StarWarsTheRevengeOfTheSith(Vehicle.Class()):
+class StarWarsTheRevengeOfTheSithPart3(Vehicle.Class()):
   apple = "banana"
 
 @pcc_set
