2610777eecd6627edf70d6d9df0ecc2851b66c4b
Arthur Valadares <arthur00@gmail.com>
2016-05-26 17:43:15 -0700

    solved deadlock?

--- datamodel/amazoncar/mydatamodel.py
+++ datamodel/amazoncar/mydatamodel.py
@@ -25,7 +25,7 @@ logger = logging.getLogger(__name__)
 LOG_HEADER = "[DATAMODEL]"
 
 @pcc_set
-class KiwiCar(Vehicle.Class()):
+class PauseUnpauseCar(Vehicle.Class()):
   apple = "banana"
 
 @pcc_set
