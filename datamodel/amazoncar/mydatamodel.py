5a5a09036f22732ea160318cb6dcd4b7ed680685
刘吉琳 <LiuJilin@dhcp-v029-099.mobile.uci.edu>
2016-06-08 16:10:32 -0700

    My amazon car data model

--- datamodel/amazoncar/mydatamodel.py
+++ datamodel/amazoncar/mydatamodel.py
@@ -16,7 +16,7 @@ from pcc.projection import projection
 from pcc.set import pcc_set
 from pcc.attributes import dimension, primarykey
 from datamodel.nodesim.datamodel import Waypoint
-from datamodel.common.datamodel import Vehicle, Vector3, Color
+from datamodel.common.datamodel import Vehicle, Vector3
 
 import traceback
 
@@ -31,6 +31,7 @@ class AmazonCar(Vehicle.Class()):
   '''
 
   _Aflag = True
+  SPEED = 5
   @dimension(bool)
   def Aflag(self):
     return self._Aflag
@@ -51,6 +52,7 @@ class AmazonCar(Vehicle.Class()):
     self.Name = ""
     self.Aflag = True
     self.Waypoints = []
+    self.FinalPosition = {}
 
   @dimension(list)
   def Waypoints(self):
@@ -60,9 +62,25 @@ class AmazonCar(Vehicle.Class()):
   def Waypoints(self, value):
     self._Waypoints = value
 
+  @dimension(int)
+  def CurrentNode(self):
+    return self._CurrentNode
+
+  @CurrentNode.setter
+  def CurrentNode(self, value):
+    self._CurrentNode = value
+
+  @dimension(list)
+  def FinalPosition(self):
+    return self._FinalPosition
+
+  @FinalPosition.setter
+  def FinalPosition(self, value):
+    self._FinalPosition = value
+
 # Amazon Inactive
 @subset(AmazonCar)
-class AmazonInactiveCar(AmazonCar):
+class AmazonInactiveCar(AmazonCar.Class()):
     @staticmethod
     def __query__(cars):
         return [c for c in cars if AmazonInactiveCar.__predicate__(c)]
@@ -73,11 +91,12 @@ class AmazonInactiveCar(AmazonCar):
 
     def start(self):
       logger.debug("[AmazonInactiveCar]: {0} starting".format(self.ID))
+      self.CurrentNode = 0
       self.Velocity = Vector3(self.SPEED, 0, 0)
 
 # Amazon InactiveCar
 @subset(AmazonCar)
-class AmazonActiveCar(AmazonCar):
+class AmazonActiveCar(AmazonCar.Class()):
   @staticmethod
   def __query__(cars):  # @DontTrace
     return [c for c in cars if AmazonActiveCar.__predicate__(c)]
@@ -88,9 +107,11 @@ class AmazonActiveCar(AmazonCar):
 
   def move(self, node1, node2):
         # by Jilin, based on road line from node1(x1, y1) to node2(x2, y2): y = ax + b,
+        node2 = [node2['X'], node2['Y']]
+        node1 = [node1['X'], node1['Y']]
         a = (node2[1] - node1[1])/(node2[0] - node1[0])
         b = 1
-        alpha = 1.0/50  # type int and float
+        alpha = 1.0  # type int and float
         if node2[0] > node1[0]:
           newPositionX = self.Position.X + alpha * self.SPEED / math.sqrt(pow(a, 2) + 1)
           if newPositionX > node2[0]:
@@ -117,7 +138,7 @@ class AmazonActiveCar(AmazonCar):
         # logger.debug("[ActiveCar] {2}: Current velocity: {0}, New position {1}".format(self.Velocity, self.Position, self.ID))
 
         # End of ride
-        if (self.Position.X >= self.FINAL_POSITION) or (self.Position.Y >= self.FINAL_POSITION):
+        if (self.Position.X == self.FinalPosition[0]) or (self.Position.Y == self.FinalPosition[1]):
             self.stop()
 
 
